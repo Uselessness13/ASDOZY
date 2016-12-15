c12fdb9c882e1f699108a360c6807d6bfa472359
Andrew <andrew13131336@gmail.com>
2016-12-15 12:48:28 +0300

    lsrg

--- АСДОЗУ/Form1.cs
+++ АСДОЗУ/Form1.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dabse = System.Data.OleDb;
 
 namespace АСДОЗУ
 {
     public partial class Form1 : Form
     {
+        dabse.OleDbConnection connection = new dabse.OleDbConnection("Data Source=\"ДОУ.mdb\";User " + "ID=Admin;Provider=\"Microsoft.Jet.OLEDB.4.0\";");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+
         }
     }
 }
